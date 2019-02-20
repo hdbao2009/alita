b66c46a2a02db0de3bdf70e0749b274ced4a95b9
hdbao2009 <39456172+hdbao2009@users.noreply.github.com>
2019-02-20 10:03:10 +0700

    remove S add R

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-const port = process.env.POST || 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log('ahihi');
