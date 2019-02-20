3f141e330ca78045c666399ff9ec188707dfb6bb
huynh dinh bao <bao.hd@sutrixsolutions.com>
2019-02-20 10:26:45 +0700

    dasdas

---
+++ constant.js
@@ -0,0 +1,10 @@
+const url = "http://localhost:3000/";
+let EN_POINT = {
+  link: {
+    Posts: url + "posts/",
+    Tags: url + "tags/",
+    Categories: url + "categories/",
+  }
+};
+
+module.exports = EN_POINT;
