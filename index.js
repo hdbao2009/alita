f6eef47dde66b69328e65d3b2add3c4fc5a47641
huynh dinh bao <bao.hd@sutrixsolutions.com>
2019-02-21 09:58:30 +0700

    asdasd

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ var express = require('express'),
   cron = require('node-cron');
 const port = process.env.PORT || 8000;
   
-let postsRoutes = require('./router/postsRouter');
-let tagsRoutes = require('./router/tagsRouter');
-let categoryRoutes = require('./router/categoryRouter');
+// let postsRoutes = require('./router/postsRouter');
+// let tagsRoutes = require('./router/tagsRouter');
+// let categoryRoutes = require('./router/categoryRouter');
 
 var app = express();
 
-var staticDir = express.static(
-	path.resolve(__dirname, 'public')
-);
-app.use(staticDir);
+// var staticDir = express.static(
+// 	path.resolve(__dirname, 'public')
+// );
+// app.use(staticDir);
 
 // app.use(express.static('dist'));
 // app.get('*', (request, response) => {
 // response.sendFile(path.join(__dirname, 'dist', 'index.html'));
 // });
 
-app.use(cors());
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(bodyParser.json());
 
 app.get('/',(req, res) => {
 	res.send("Home");
