fcd0682874322058c165b1e0cd4889e5e421ee8e
huynh dinh bao <bao.hd@sutrixsolutions.com>
2019-02-21 14:27:09 +0700

    adsad

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 const port = process.env.PORT || 8000;
 
 var app = express();
+app.listen(port, () => {
+	console.log('ahihi');
+});
 
 app.get('/',(req, res) => {
+	
 	res.send("Home");
 })
 app.get('/posts',(req, res) => {
 	res.send("Posts");
 })
-
-app.listen(port, () => {
-  console.log('ahihi');
-});
