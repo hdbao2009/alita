6ba4bcc1cbbb4288d17798ff6cf0398dbefe4bfd
hdbao2009 <39456172+hdbao2009@users.noreply.github.com>
2019-03-22 11:01:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,11 @@ app.use(staticDir);
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
 	res.send("Home");
