5714780de9987a2589e8510a81b1530b1e5a5acd
CocaPepsi <hdbao2009@gmail.com>
2019-03-23 17:36:58 +0700

    upload

--- controllers/upLoadFileController.js
+++ controllers/upLoadFileController.js
@@ -11,24 +11,24 @@ module.exports = {
 
 	uploadImgContent: function (req, res) {
 		console.log(res);
-		
-		FroalaEditor.Image.upload(req, '/public/imagePosts/', async function (err, data) {
-			console.log(data);
+		res.send(res);
+		// FroalaEditor.Image.upload(req, '/public/imagePosts/', async function (err, data) {
+		// 	console.log(data);
 			
-			const fileName = data.link.split('/')[3];
-			let customData = {
-				originalname: "postContent_" + fileName,
-				path: data.link.substring(1, data.link.length)
-			}
-			let idDriveIMGPost = await uploadFileToDrive(customData);
-			if (err) {
-				return res.send(JSON.stringify(err));
-			}
+		// 	const fileName = data.link.split('/')[3];
+		// 	let customData = {
+		// 		originalname: "postContent_" + fileName,
+		// 		path: data.link.substring(1, data.link.length)
+		// 	}
+		// 	let idDriveIMGPost = await uploadFileToDrive(customData);
+		// 	if (err) {
+		// 		return res.send(JSON.stringify(err));
+		// 	}
 
-			let link = process.env.LINK_UPLOAD || 'http://localhost:8000'
-			data['link'] = 'https://drive.google.com/uc?id=' + idDriveIMGPost.data.id
+		// 	let link = process.env.LINK_UPLOAD || 'http://localhost:8000'
+		// 	data['link'] = 'https://drive.google.com/uc?id=' + idDriveIMGPost.data.id
 			
-			res.send(data);
-		});
+		// 	res.send(data);
+		// });
 	}
 }
