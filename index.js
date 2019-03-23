b1fcb07ea9fc56d10deda7c973711316246af1e8
CocaPepsi <hdbao2009@gmail.com>
2019-03-23 09:42:42 +0700

    upload code

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const port 				= process.env.PORT || 8000;
 
 var app = express();
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
 });
 
 mongoose.set('useCreateIndex', true);
