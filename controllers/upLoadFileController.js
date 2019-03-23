6365ac0c55b25b69c50e6e8bf991c33e504ef2b1
CocaPepsi <hdbao2009@gmail.com>
2019-03-23 16:40:06 +0700

    upload code

--- controllers/upLoadFileController.js
+++ controllers/upLoadFileController.js
@@ -9,18 +9,22 @@ module.exports = {
 		});
 	},
 
-	uploadImgContent: async function (req, res) {
-	 	let data = await FroalaEditor.Image.upload(req, '/public/imagePosts/');
-		const fileName = data.link.split('/')[3];
-		let customData = {
-			originalname: "postContent_" + fileName,
-			path: data.link.substring(1, data.link.length)
-		}
-		let idDriveIMGPost = await uploadFileToDrive(customData);
-		if (err) {
-			return res.send(JSON.stringify(err));
-		}
-		data['link'] = 'https://drive.google.com/uc?id=' + idDriveIMGPost.data.id
-		res.send(data);
+	uploadImgContent: function (req, res) {
+		FroalaEditor.Image.upload(req, '/public/imagePosts/', async function (err, data) {
+			const fileName = data.link.split('/')[3];
+			let customData = {
+				originalname: "postContent_" + fileName,
+				path: data.link.substring(1, data.link.length)
+			}
+			let idDriveIMGPost = await uploadFileToDrive(customData);
+			if (err) {
+				return res.send(JSON.stringify(err));
+			}
+
+			let link = process.env.LINK_UPLOAD || 'http://localhost:8000'
+			data['link'] = 'https://drive.google.com/uc?id=' + idDriveIMGPost.data.id
+			
+			res.send(data);
+		});
 	}
 }
