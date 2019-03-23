9b9a354e7ce58b095161b02b6d0be0c04e4f65b2
CocaPepsi <hdbao2009@gmail.com>
2019-03-23 23:30:04 +0700

    upload code

--- controllers/upLoadFileController.js
+++ controllers/upLoadFileController.js
@@ -15,6 +15,7 @@ module.exports = {
 			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 			res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 			res.setHeader('Access-Control-Allow-Credentials', true);
+			res.setHeader('Content-Length', 0);
 			const fileName = data.link.split('/')[3];
 			let customData = {
 				originalname: fileName,
