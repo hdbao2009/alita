08f6e5acde62c91f24b007e7050217d0b32db1a5
CocaPepsi <hdbao2009@gmail.com>
2019-04-02 00:31:38 +0700

    upload code

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,12 +1,7 @@
 let postsModel 			= require("../models/postsModel");
-let tagsModel 			= require("../models/tagsModel");
-let en_point 				= require("../constant");
 let moment 					= require('moment');
-let _ 							= require('lodash');
-let path					 	= require('path')
-const uploadFileToDrive = require('../repo/uploadFileToDrive');
 const lazyLoadPosts = require('../repo/lazyLoadPosts');
-const checkTags = require('../repo/checkTags');
+const checkTags 		= require('../repo/checkTags');
 
 module.exports = {
 	// Show list posts
@@ -22,17 +17,11 @@ module.exports = {
 			req.body.listIdTags = await checkTags(req.body.listIdTags);
 			let posts = new postsModel(req.body);
 			posts.createDate = moment(new Date()).format('DD/MM/YYYY, h:mm:ss');
-			posts.save().then(result => {
-				res.status(201).json({
-					message: "created posts successfully",
-					success: result,
-					status: 1
-				});
-			}).catch(err => {
-				res.status(500).json({
-					message: 'Error when creating posts',
-					error: err
-				});
+			let result = await posts.save();
+			res.status(201).json({
+				message: "created posts successfully",
+				success: result,
+				status: 1
 			});
 		} catch (error) {
 			res.status(500).json({
@@ -40,11 +29,10 @@ module.exports = {
 				error: error
 			});
 		}
-		
 	},
 
 	// Get posts by id
-	getPostsById: (req, res) => {
+	getPostsById: async (req, res) => {
 		let id = req.params.id;
 		//TODO: Plan one
 		// postsModel.findOne({ _id: id }, (err, post) => {
@@ -52,65 +40,54 @@ module.exports = {
 		// });
 
 		//TODO: Plan two
-		postsModel.findById(id)
-			.select()
-			.exec()
-			.then(result => {
-				if (result) {
-					res.status(200).json({
-						message: 'get posts successfully',
-						post: result
-					})
-				} else {
-					res.status(404).json({
-						message: 'Not valid entry found for provider ID',
-						error: err
-					})
-				}
-			}).catch(err => {
-				res.status(500).json({
-					message: '',
-					error: err
-				})
-			});
+		try {
+			let result = await postsModel.findById(id).select().exec();
+			res.status(200).json({
+				message: 'get posts successfully',
+				success: result
+			})
+		} catch (error) {
+			res.status(500).json({
+				message: '',
+				error: error
+			})
+		}
 	},
 
 
 	// Update posts by Id
-	updatePostById: (req, res) => {
-		let id = req.params.id;
-		postsModel.update({_id: id}, {$set: req.body})
-			.exec().then(result => {
-				res.status(200).json({
-					message: "updated posts successfully",
-					success: result,
-					status: 1
-				})
-			}).catch(err => {
-				res.status(500).json({
-					message: '',
-					error: err
-				})
-			});
+	updatePostById: async (req, res) => {
+		try {
+			let id = req.params.id;
+			let result = await postsModel.update({_id: id}, {$set: req.body}).exec();
+			res.status(200).json({
+				message: "updated posts successfully",
+				success: result,
+				status: 1
+			})
+		} catch (error) {
+			res.status(500).json({
+				message: '',
+				error: error
+			})
+		}
 	},
 
 	// Delete post by Id
-	deletePostById: (req, res) => {
-		let id = req.params.id;
-		postsModel.deleteOne({
-				_id: id
+	deletePostById: async (req, res) => {
+		try {
+			let id = req.params.id;
+			let result = await postsModel.deleteOne({_id: id}).exec();
+			res.status(200).json({
+				message: 'Deleted Post Successfully',
+				success: result,
+				status: 1
 			})
-			.exec()
-			.then(result => {
-				res.status(200).json({
-					message: 'Deleted Post Successfully',
-					status: 1
-				})
-			}).catch(err => {
-				res.status(500).json({
-					message: '',
-					error: err
-				})
-			});
+		} catch (error) {
+			res.status(500).json({
+				message: '',
+				error: error
+			})
+		}
 	}
 }
